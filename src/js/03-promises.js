bf316248b9a13fdbd73f022bcd3b873530afa5a5
annamelya2021 <anna.basyuk@gmail.com>
2022-02-20 13:33:21 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,29 @@
-const form = document.querySelector('.form');
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
+// const form = document.querySelector('.form');
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  let firstDelay = Number(e.target.elements.delay.value);
-  const amountValue = e.target.elements.amount.value;
-  for (let i = 1; i <= amountValue; i += 1) {
-    createPromise(i, firstDelay)
-      .then(({ position, delay }) => {
-        console.log(`Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`Rejected promise ${position} in ${delay}ms`);
-      });
-    firstDelay += Number(e.target.elements.step.value);
-  }
-});
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   let firstDelay = Number(e.target.elements.delay.value);
+//   const amountValue = e.target.elements.amount.value;
+//   for (let i = 1; i <= amountValue; i += 1) {
+//     createPromise(i, firstDelay)
+//       .then(({ position, delay }) => {
+//         console.log(`Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         console.log(`Rejected promise ${position} in ${delay}ms`);
+//       });
+//     firstDelay += Number(e.target.elements.step.value);
+//   }
+// });
